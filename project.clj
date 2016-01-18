becc090146911f2b823a75fb0f6a1f4e0d9da329
Antony Woods <antony@mastodonc.com>
2016-01-18 15:37:40 +0000

    Updated project release tasks

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :release-tasks [["change" "version"
                    "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
-                  ["vcs" "tag" "release-"]
+                  ["vcs" "tag" "release-v"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]])
