236d3d933329ca64f65d5f9e3572469bc7c52450
Antony Woods <antony@mastodonc.com>
2016-01-18 15:00:42 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :release-tasks [["vcs" "assert-committed"]
+  :release-tasks [;["vcs" "assert-committed"]
                   ["change" "version"
                    "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
