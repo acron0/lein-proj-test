c04166b68b9a0b77d3997a7fff09406aab9bcfac
Antony Woods <antony@mastodonc.com>
2016-01-19 13:14:59 +0000

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject test-proj "0.1.5-SNAPSHOT"
+(defproject test-proj "0.1.5"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :release-tasks [["change" "version"
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
                    "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "release-v"]
